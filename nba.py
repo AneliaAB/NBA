5c66c898e4d4884a05b771d7ce7ab2e3270d94dc
“AneliaAB” <aneliabg10@gmail.com>
2023-11-06 21:50:52 +0100

    fixing paths

--- nba.py
+++ nba.py
@@ -18,23 +18,24 @@ class DC_Chapter_Spider(scrapy.Spider):
     link_path = response.xpath('//th[@data-stat="season"]/a')
     date_path_ext = link_path.xpath('./text()').extract()
     seasons.append(date_path_ext)
-    links_to_follow = link_path.xpath('./@href').extract()
+    links_to_follow = link_path.xpath('./@href')
     for url in links_to_follow:
         yield response.follow(url = url,
                             callback = self.parse_results)
         time.sleep(2)
 
   def parse_results(self, response):
-    link_to_follow = response.xpath('//ul[@class="hoversmooth"]/li[3]/a/@href').extract()
-    link_to_follow_list.append(link_to_follow)
-    yield response.follow(url = link_to_follow,
-                            callback = self.parse_filter)
+    link_to_follow = response.xpath('///ul[@class="hoversmooth"]/li[3]/a/@href')
+    for url in link_to_follow:
+        yield response.follow(url = url,
+                                callback = self.parse_filter)
     time.sleep(2)
+    link_to_follow_list.append(link_to_follow)
 
   def parse_filter(self, response):
     #header = response.xpath('//h1/span[@class="header_end"]/text()')
     #header_list.append(header)
-    filter_path = response.xpath('//div[@class="filter"]//div/a/@href').extract()
+    filter_path = response.xpath('//div[@class="filter"]//div/a/@href')
     filter_path_list.append(filter_path)
     yield response.follow(url = filter_path,
                             callback = self.parse_table)
@@ -66,13 +67,15 @@ arena_list = []
 visitor_list = []
 
 dict_data = dict()
+
 # Run the Spider
 process = CrawlerProcess()
 process.crawl(DC_Chapter_Spider)
 process.start()
 
+print('Try out')
 print(seasons)
-print(link_to_follow_list)
+print("link to follow" + link_to_follow_list[1:5])
 print(arena_list[1:5])
 print(filter_path_list)
 print(visitor_list[1:5])
